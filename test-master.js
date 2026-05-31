4e3fd3541fac032782efc514993145d7f2423e2d
antheasmith07-png <anthea.smith07@gmail.com>
2026-05-30 19:54:15 -0500

    AS_5.30.26

--- test-master.js
+++ test-master.js
@@ -1,41 +1,58 @@
 describe("Be the Test Master", () => {
   // Test #1
+  //I: replace the ???
+  //O: The value inputted should make the tests pass
+  //C: n/a
+  //E: n/a
   it("Our First Test: Replace the ??? to make the tests pass", function () {
     let stringValue = "I am a string";
     let numberValue = 484;
 
-    assert.equal(stringValue, "???");
-    assert.equal(numberValue, "???");
+    assert.equal(stringValue, "I am a string")
+    assert.equal(numberValue, 484);
   });
 
   // Test #2
+  //I: a function that accesses a value outside of the function
+//O: return a value that is outside of the function
+//C: n/a
+//E: n/a
   it("Functions can access/modify variables from a parent scope", () => {
     var outside_the_function = null;
 
     function yay() {
       var inside_the_function = "can you see me?";
+      //By making outside = inside the function both should return "can you see me?"
       outside_the_function = inside_the_function;
     }
 
     yay();
 
-    assert.equal(outside_the_function, "???");
+    assert.equal(outside_the_function, "can you see me?");
   });
 
   // Test #3
+  //I: a function with parameters that is scoped to that function
+  //O: a value that is scoped to the function
+  //C: n/a
+  //E: n/a
   it("Function parameters become scoped to the function", () => {
     function yay(param) {
-      assert.equal(param, "???");
+      assert.equal(param, "a fine kettle of fish");
     }
 
     yay("a fine kettle of fish");
   });
 
   // Test #4
+   //I: a function with a local scope tht cannot be accessed by the outer scope
+  //O: a value that is scoped to the function
+  //C: n/a
+  //E: n/a
   it("A functions local scope is not available in an outer scope.", () => {
     function yay() {
       var kix = "kid tested mother approved";
-      assert.equal(kix, "???");
+      assert.equal(kix, "kid tested mother approved");
     }
     yay();
 
@@ -49,10 +66,14 @@ describe("Be the Test Master", () => {
     } else {
       has_kix = "i prefer cheerios";
     }
-    assert.equal(has_kix, "???");
+    assert.equal(has_kix, "i prefer cheerios");
   });
 
   // Test #5
+   //I: a function with parameters that don't have access to each other's scope
+  //O: a value that is scoped to their particular function
+  //C: n/a
+  //E: n/a
   it("Functions don't have access to each others scope", () => {
     function yay() {
       var from_yay = "i'm inside yay;";
@@ -63,28 +84,36 @@ describe("Be the Test Master", () => {
       if (this.from_yay !== undefined) {
         in_foo = this.from_yay;
       }
-      assert.equal(in_foo, "???");
-      assert.equal(this.from_yay, "???");
+      assert.equal(in_foo, "i'm in foo");
+      assert.equal(this.from_yay, undefined);
     }
     yay();
     foo();
   });
 
   // Test #6
+   //I: a function with inner scope variables overrides outter scope variables
+  //O: a value that overrides the outer scope variable
+  //C: n/a
+  //E: n/a
   it("Inner scope variables override outter scope variables.", () => {
     var peanuts = 300;
 
     function yay() {
       var peanuts = "roasted";
 
-      assert.equal(peanuts, "???");
+      assert.equal(peanuts, "roasted");
     }
     yay();
 
-    assert.equal(peanuts, "???");
+    assert.equal(peanuts, 300);
   });
 
   // Test #7
+   //I: a function with var in the function repeats its function
+  //O: a value that is repeatable
+  //C: n/a
+  //E: n/a
   it("Variables created with var in a funtion are re-created each time", () => {
     function yay() {
       if (this.counter !== undefined) {
@@ -95,14 +124,18 @@ describe("Be the Test Master", () => {
     }
 
     yay();
-    assert.equal(this.counter, "???");
+    assert.equal(this.counter, undefined);
     yay();
-    assert.equal(this.counter, "???");
+    assert.equal(this.counter, undefined);
     yay();
-    assert.equal(this.counter, "???");
+    assert.equal(this.counter, undefined);
   });
 
   // Test #8
+   //I: a function.s inner scope can access the outer scope
+  //O: return an outer scope value
+  //C: n/a
+  //E: n/a
   it("Inner scope can access outer scope", () => {
     var im_outside = "alpha";
     function yay() {
@@ -110,10 +143,14 @@ describe("Be the Test Master", () => {
       return im_outside + im_inside;
     }
 
-    assert.equal(yay(), "???");
+    assert.equal(yay(), "alphaomega");
   });
 
   // Test #9
+   //I: a function that retains outer scope
+  //O: a value that is scoped to the function
+  //C: n/a
+  //E: n/a
   it("Functions retain outer scope references between calls.", () => {
     var im_outside = 13;
     function yay() {
@@ -121,12 +158,16 @@ describe("Be the Test Master", () => {
     }
 
     yay();
-    assert.equal(im_outside, "???");
+    assert.equal(im_outside, 14);
     yay();
-    assert.equal(im_outside, "???");
+    assert.equal(im_outside, 15);
   });
 
   // Test #10
+   //I: a function that does goofy stuff with outer scope
+  //O: a value that is goofy
+  //C: n/a
+  //E: n/a
   it("We can do goofy stuff with outer scope", () => {
     var hello = "greg";
     var name = "";
@@ -136,14 +177,18 @@ describe("Be the Test Master", () => {
     }
 
     yay();
-    assert.equal(name, "???");
+    assert.equal(name, "greg");
     yay();
-    assert.equal(name, "???");
+    assert.equal(name, "greggreg");
     yay();
-    assert.equal(name, "???");
+    assert.equal(name, "greggreggreg");
   });
 
   // Test #11
+   //I: a function to other functions
+  //O: run those functions
+  //C: n/a
+  //E: n/a
   it("We can pass functions to other functions and then run them.", () => {
     var im_outter = 10;
     function yay() {
@@ -154,10 +199,14 @@ describe("Be the Test Master", () => {
       whatever();
     }
     something(yay);
-    assert.equal(im_outter, "???");
+    assert.equal(im_outter, 40);
   });
 
   // Test #12
+   //I: a function that gets crazy with returns
+  //O: a value that is crazy
+  //C: n/a
+  //E: n/a
   it("We can get crazy with returns.", () => {
     function yay() {
       return " is dog";
@@ -165,6 +214,6 @@ describe("Be the Test Master", () => {
     function foo(whatever) {
       return "hello, this" + whatever();
     }
-    assert.equal(foo(yay), "???");
+    assert.equal(foo(yay), "hello, this is dog");
   });
 });
